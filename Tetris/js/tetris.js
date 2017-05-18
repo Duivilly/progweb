c52a46f23b9cf4b87f77c0c37823e3b651cd0997
Duivilly Brito <db@icomp.ufam.edu.br>
2017-05-18 18:30:57 -0400

    testes

--- Tetris/js/tetris.js
+++ Tetris/js/tetris.js
@@ -1,15 +1,32 @@
 (function () {
 
-    var tabuleiro;
-    var peca;
-    var fps= 2;
-    var gameLoop;
-    var controle= 6;
+    init();
 
+    //fazer closures//
+    function init(){
+        //o tabuleiro
+        tabuleiro= "";
+        //nova peca
+        peca= "";
+        //execucao do jogo
+        gameLoop= "";
+        //controle de left e rigth
+        controll= 6;
+        //controla a velocidade
+        fps= 1;
+        //cria o tabuleiro
+        createTabuleiro();
+        //inicia o jogo
+        start();
+    }
+
+    //cria o tabuleiro com uma tabela
     function createTabuleiro(){
         table= document.createElement("table");
+        //cor dos blocos do tabuleiro
         table.style.backgroundColor= "#FFFACD";
-        table.border= "0";
+        //borda da tabela
+        table.border= "1";
         createLinhaTabuleiro();
         createLinhaTabuleiro();
         createLinhaTabuleiro();
@@ -27,6 +44,7 @@
         document.getElementById("tabuleiro").appendChild(table);
     }
 
+    //cria as linhas da tabela
     function createLinhaTabuleiro(){
         tr= document.createElement("tr");
         createColunaTabuleiro();
@@ -45,68 +63,70 @@
         table.appendChild(tr);
     }
 
+    //cria as colunas da tabela
     function createColunaTabuleiro(){
         td= document.createElement("td");
+        //tamanho de cada bloco do tabuleiro
         td.width= "20";
         td.height= "20";
         tr.appendChild(td);
     }
 
+    //controle de posicoes das pecas
     addEventListener("keydown", function(e) {
-
+        //up arrow
         if (e.keyCode == '38') {
-            //up arrow
             //peca.style.top = (parseInt(peca.style.top) - 10) + "px";
         }
+        //down arrow
         else if (e.keyCode == '40') {
-            //down arrow
             //peca.style.top = (parseInt(peca.style.top) + 10) + "px";
-            if(coluna < 13){
-                coluna= coluna +1;
-            }
         }
+        //left arrow
         else if (e.keyCode == '37') {
-            //left arrow
             //peca.style.left = (parseInt(peca.style.left) - 10) + "px";
-            if(controle > 0){
-                controle= controle -1;
+            if(controll > 0){
+                controll= controll -1;
             }
         }
+        //right arrow
         else if (e.keyCode == '39') {
-            //right arrow
             //peca.style.left= (parseInt(peca.style.left) + 10) + "px";
-            if(controle < 12){
-                controle= controle +1;
+            if(controll < 12){
+                controll= controll +1;
             }
         }
     });
 
-    function init() {
-        coluna= 0;
+    //inicio do jogo
+    function start() {
+        linha= 0;
+        //createPeca();
         gameLoop= setInterval(run, 1000/fps);
     }
 
     function run() {
-        if(parseInt(coluna) < 13){
-            createPeca();
-            coluna= coluna + 1;
-            console.log(coluna);
-        }else{
-            clearInterval(gameLoop);
-            init();
+        //console.log('px:'+peca.style.backgroundColor);
+        //peca.style.top= linha +"px";
+        if (linha < 13) {
+            table.rows[linha].cells[controll-1].style.backgroundColor= "green";
+            table.rows[linha].cells[controll].style.backgroundColor= "green";
+            table.rows[linha].cells[controll+1].style.backgroundColor= "green";
+            if(linha != 0){
+                table.rows[linha-1].cells[controll-1].style.backgroundColor= "#FFFACD";
+                table.rows[linha-1].cells[controll].style.backgroundColor= "#FFFACD";
+                table.rows[linha-1].cells[controll+1].style.backgroundColor= "#FFFACD";
+            }
+            linha= linha + 1;
+            console.log(linha);
         }
     }
 
     function createPeca() {
-        table.rows[coluna].cells[controle-1].style.background= "yellow";
-        table.rows[coluna].cells[controle].style.background= "yellow";
-        table.rows[coluna].cells[controle+1].style.background= "yellow";
+        proximaPeca= document.getElementById('proxima-peca');
+        peca= document.createElement('div');
+        peca.style.backgroundColor= "orange";
+        proximaPeca.appendChild(peca);
     }
 
-    function limpar() {
-        table.rows[coluna-1].cells[controle].style.background= "#FFFACD";
-    }
-
-    createTabuleiro();
-    init();
 })();
