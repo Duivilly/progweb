3c28892a6a66cf74daff140a7312789d28ae6c83
Duivilly Brito <db@icomp.ufam.edu.br>
2017-05-27 15:16:09 -0400

    nova versão

--- Tetris/js/tetris.js
+++ Tetris/js/tetris.js
@@ -4,22 +4,32 @@
 
     //fazer closures//
     function init(){
+        //execucao do jogo
+        gameLoop= null;
+        //linhas
+        lines= 4;
+        //controle de left e rigth
+        controll= 0;
+        //ups
+        girarI= true;
+        girarS= true;
+        girarL= [true, false, false, false];
+        girarT= [true, false, false, false];
+        //controla a velocidade
+        fps= 1;
         //cria o tabuleiro
         createTabuleiro();
-        //controle
-        controll= 0;
         //inicia o jogo
         start();
     }
 
     //cria o tabuleiro com uma tabela
     function createTabuleiro(){
-        tabela= document.createElement("table");
+        table= document.createElement("table");
         //cor dos blocos do tabuleiro
-        tabela.id= "tabela";
-        tabela.style.backgroundColor= "#5F9EA0";
+        table.style.backgroundColor= "#5F9EA0";
         //borda da tabela
-        tabela.border= "0";//10x7
+        table.border= "1";
         createLinhaTabuleiro();
         createLinhaTabuleiro();
         createLinhaTabuleiro();
@@ -29,8 +39,10 @@
         createLinhaTabuleiro();
         createLinhaTabuleiro();
         createLinhaTabuleiro();
-        createLinhaTabuleiro(); 
-        document.getElementById("tabuleiro").appendChild(tabela);
+        createLinhaTabuleiro();
+        createLinhaTabuleiro();
+        createLinhaTabuleiro();
+        document.getElementById("tabuleiro").appendChild(table);
     }
 
     //cria as linhas da tabela
@@ -43,7 +55,12 @@
         createColunaTabuleiro();
         createColunaTabuleiro();
         createColunaTabuleiro();
-        tabela.appendChild(tr);
+        createColunaTabuleiro();
+        createColunaTabuleiro();
+        createColunaTabuleiro();
+        createColunaTabuleiro();
+        createColunaTabuleiro();
+        table.appendChild(tr);
     }
 
     //cria as colunas da tabela
@@ -55,298 +72,166 @@
         tr.appendChild(td);
     }
 
-    i= true;
-    s= true;
-
     //controle de posicoes das pecas
     addEventListener("keydown", function(e) {
         //up arrow
         if (e.keyCode == '38') {
-            if(i){
-                limpaI1();
-                I2();
-                i= false;
-            }else{
-                limpaI2();
-                I1();
-                i= true;
+            if(table.id == "i1" || table.id == "i2"){
+                if(girarI){
+                    table.id= "i2";
+                    girarI= false;
+                }else{
+                    table.id= "i1";
+                    girarI= true;
+                }
             }
 
-            if(s){
-                limpaS1();
-                S2();
-                s= false;
-            }else{
-                limpaS2();
-                S1();
-                s= true;
+            if(table.id == "s1" || table.id == "s2"){
+                if(girarS){
+                    table.id= "s2";
+                    girarS= false;
+                }else{
+                    table.id= "s1";
+                    girarS= true;
+                }
             }
-            
+
+            if(table.id == "l1" || table.id == "l2" || table.id == "l3" || table.id == "l4"){
+                if(girarL[0]){
+                    table.id= "l2";
+                    girarL[1]= true;
+                    girarL[0]= false;
+                }else if(girarL[1]){
+                    table.id= "l3";
+                    girarL[2]= true;
+                    girarL[1]= false;
+                }else if(girarL[2]){
+                    table.id= "l4";
+                    girarL[3]= true;
+                    girarL[2]= false;
+                }else{
+                    table.id= "l1";
+                    girarL[0]= true;
+                    girarL[3]= false;
+                }
+            }
+
+            if(table.id == "t1" || table.id == "t2" || table.id == "t3" || table.id == "t4"){
+                if(girarT[0]){
+                    table.id= "t2";
+                    girarT[1]= true;
+                    girarT[0]= false;
+                }else if(girarT[1]){
+                    table.id= "t3";
+                    girarT[2]= true;
+                    girarT[1]= false;
+                }else if(girarT[2]){
+                    table.id= "t4";
+                    girarT[3]= true;
+                    girarT[2]= false;
+                }else{
+                    table.id= "t1";
+                    girarT[0]= true;
+                    girarT[3]= false;
+                }
+            }
         }
         //down arrow
         else if (e.keyCode == '40') {
-            down= down + 1;
+            
         }
         //left arrow
         else if (e.keyCode == '37') {
-            if(controll > 0){
-                controll= controll - 1;
-            }
+            controll= controll -1;
         }
         //right arrow
         else if (e.keyCode == '39') {
-            if(controll < 10){
-                controll= controll + 1;
-            }
+            controll= controll + 1;
         }
     });
 
     //inicio do jogo
     function start() {
         down= 0;
-        downAntes= 0;
-        controllAntes= 0;
-        colisao= null;
-        peca= "I1";
-        gameLoop= setInterval(run, 1000);
+        proximaPeca();
+        //random
+        proximaPeca.id= "i1";
+        gameLoop= setInterval(run, 1000/fps);
     }
 
+    jogadas= [];
+
     function run() {
-        //colisaoQT= tabela.rows[down+1].cells[controll+1].style.backgroundColor;
-        //colisaoL= tabela.rows[down+2].cells[controll+1].style.backgroundColor;
-        //colisaoL1= tabela.rows[down+2].cells[controll].style.backgroundColor;
-        //colisaoI= tabela.rows[down].cells[controll].style.backgroundColor;
-        if(down < 11){
-            if(down != 0){
-                if(peca == "I1"){
-                    limpaI1();
-                }else if(peca == "I2"){
-                    limpaI2();
-                }
-                if(peca == "S1"){
-                    limpaS1();
-                }else if(peca == "S2"){
-                    limpaS2();
+        if(down < lines){
+            //remove tabela do tabuleiro
+            document.getElementById('tabuleiro').removeChild(table);
+            //cria nova tabela
+            createTabuleiro();
+            //inseri peca atual
+            table.id= "t1";
+
+            //verifica colunas preenchidas
+            var cont= 0;
+            for(var i= 0; i < lines; i++) {
+                if(table.rows[down].cells[i].style.backgroundColor == "black"){
+                    cont= cont + 1;
                 }
             }
-            if(down < 11){//colisao
-                if(peca == "I1"){
-                    I1();
-                }else if(peca == "I2"){
-                    I2();
-                }
-                if(peca == "S1"){
-                    S1();
-                }else if(peca == "S2"){
-                    S2();
-                }
-                console.log("backgroundColor:"+tabela.rows[down+1].cells[controll+1].style.backgroundColor);
-                downAntes= down;
-                controllAntes= controll;
-                down= down + 1;
-            }else{
-                clearInterval(gameLoop);
-                start();
+            //se colunas preenchidas totalmente limpa e desce pecas
+            if(cont == lines){
+                console.log('limpa');
             }
-            console.log("agora("+down+","+controll+")"+" -- antes("+downAntes+","+controllAntes+")");
+            down= down + 1;
+            //coloca tabela no tabuleiro
+            document.getElementById('tabuleiro').appendChild(table);
         }else{
             clearInterval(gameLoop);
-            start();
+            //atualiza jogadas
+            stop= (down-1)+","+controll;
+            jogadas[0]= stop.split(',');
+            console.log(jogadas);
+            //start();
         }
     }
 
+    //cria o tabuleiro com uma tabela
     function proximaPeca(){
-
+        proximaPeca= document.createElement("table");
+        //cor dos blocos do tabuleiro
+        proximaPeca.style.backgroundColor= "orange";
+        //borda da tabela
+        proximaPeca.border= "1";
+        createLinhaProximaPeca();
+        createLinhaProximaPeca();
+        createLinhaProximaPeca();
+        document.getElementById('proxima-peca').appendChild(proximaPeca);
     }
 
-    function createPeca() {
-        
+    //cria as linhas da tabela
+    function createLinhaProximaPeca(){
+        tr= document.createElement("tr");
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        createColunaProximaPeca();
+        proximaPeca.appendChild(tr);
     }
 
-    function T1(){
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaT1(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function T2(){
-        tabela.rows[down].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaT2(){
-        tabela.rows[downAntes].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function T3(){
-        tabela.rows[down+2].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaT3(){
-        tabela.rows[downAntes+2].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function T4(){
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaT4(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function Q(){
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaQ(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function L1(){
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaL1(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function L2(){
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll].style.backgroundColor= "orange";
-    }
-
-    function limpaL2(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-    }
-
-    function L3(){
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaL3(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function L4(){
-        tabela.rows[down+1].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+2].style.backgroundColor= "orange";
-    }
-
-    function limpaL4(){
-        tabela.rows[downAntes+1].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-    }
-
-    function S1(){
-        peca= "S1";
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll+1].style.backgroundColor= "orange";
-    }
-
-    function limpaS1(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-    }
-
-    function S2(){
-        peca= "S2";
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll+2].style.backgroundColor= "orange";
-    }
-
-    function limpaS2(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-    }
-
-    function I1(){
-        peca= "I1";
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+1].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+2].style.backgroundColor= "orange";
-        tabela.rows[down].cells[controll+3].style.backgroundColor= "orange";
-    }
-
-    function limpaI1(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+1].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+2].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes].cells[controllAntes+3].style.backgroundColor= "#5F9EA0";
-    }
-
-    function I2(){
-        peca= "I2";
-        tabela.rows[down].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+1].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+2].cells[controll].style.backgroundColor= "orange";
-        tabela.rows[down+3].cells[controll].style.backgroundColor= "orange";
-    }
-
-    function SlimpaI2(){
-        tabela.rows[downAntes].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+1].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+2].cells[controllAntes].style.backgroundColor= "#5F9EA0";
-        tabela.rows[downAntes+3].cells[controllAntes].style.backgroundColor= "#5F9EA0";
+    //cria as colunas da tabela
+    function createColunaProximaPeca(){
+        td= document.createElement("td");
+        //tamanho de cada bloco do tabuleiro
+        td.width= "20";
+        td.height= "20";
+        tr.appendChild(td);
     }
 
 })();
