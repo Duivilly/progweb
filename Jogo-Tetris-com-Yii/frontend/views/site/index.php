66a5252c00540b115a6eca634306b63ed4818ccc
Duivilly Brito <db@icomp.ufam.edu.br>
2017-06-07 17:33:34 -0400

    recc

--- Jogo-Tetris-com-Yii/frontend/views/site/index.php
+++ Jogo-Tetris-com-Yii/frontend/views/site/index.php
@@ -6,12 +6,12 @@ $this->title = 'Jogo Tetris';
     <head>
         <meta charset="utf-8">
         <title>Jogo Tetris</title>
-        <link rel="stylesheet" href="/css/estilos.css">
+        <link rel="stylesheet" href="css/estilos.css">
     </head>
     <body>
         <div id="proxima-peca"></div>
         <div id="tabuleiro"></div>
         <div id="informacoes"></div>
-        <script type="text/javascript" src="/js/tetris.js"></script>
+        <script type="text/javascript" src="js/tetris.js"></script>
     </body>
 </html>
